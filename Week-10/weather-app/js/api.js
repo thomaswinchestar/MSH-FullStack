e5183065faf16a291bab72f755935fc9a950554a
thomaswinchestar <thomasanthony999@gmail.com>
2026-02-01 10:05:26 +0700

    Protect API key with environment variables

--- Week-10/weather-app/js/api.js
+++ Week-10/weather-app/js/api.js
@@ -1,7 +1,11 @@
-const API_KEY = '5b41d26463490bfe1bceb2e67caff229'
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
 export async function getWeather(city) {
+    if(!API_KEY) {
+        throw new Error('API key is not configured');
+    }
+
     const response = await fetch(
         `${BASE_URL}?q=${city}&units=metric&appid=${API_KEY}`
     );

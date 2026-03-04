ab4b0e25374768ba3603a91d68c355a6c03e24ff
thomaswinchestar <thomasanthony999@gmail.com>
2026-03-04 20:55:31 +0700

    php env and setup configurations

---
+++ Week-12/PHP-Basic-to-Advanced/php-demo/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hello World!";
+echo "<br>";
+echo "Welcome to PHP Programming";
+echo "<br>";
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "The time is " . date("h:i:sa") . "<br>";
+?>

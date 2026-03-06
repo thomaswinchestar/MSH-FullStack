cdb911927a63ee112c5541b982bdb0ae93e93528
thomaswinchestar <thomasanthony999@gmail.com>
2026-03-06 20:11:14 +0700

    week 14 php syntax, variable 001

---
+++ Week-14/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Home Page</h1>
+    <?php
+    $num1 = 10;
+    $num2 = 20;
+    ?>
+    <p>
+        Total: <?= $num1 + $num2 ?> //Output Tag don't need to write echo and end with semicolon, short opening tag with equal sign
+    </p>
+</body>
+</html>
